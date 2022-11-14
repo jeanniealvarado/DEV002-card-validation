019e1469a0e53a5051717f9dc24bdb8312172e01
jeannie alvarado <jeaniie38@gmail.com>
2022-11-14 07:54:21 -0600

    termine el algoritmo y el maskify, logro separar y que me funcionaran ambos JS, llamando del dom a mis datos que queria ser evaluados y enmascarados

--- src/index.js
+++ src/index.js
@@ -1,54 +1,24 @@
-// import validator from './validator.js';
+ import validator from './validator.js';
 
-// console.log(validator);2
-const validar= document.getElementById("btn")
-validar.addEventListener("click", numerosEnteros)
-const tarjeta= document.getElementById ("numeros")
-function numerosEnteros() {
-    let numeroTarjeta= tarjeta.value
-    console.log(numeroTarjeta,'numero de tarjeta');
-    let a= numeroTarjeta.split("") //separar valores con el split
-    console.log(a)
-    let arrayA=[] //asignando un array vacio en el que se ingresaran los datos con valor numerico posteriormente
-    let arrayB=[] 
-    for (let i = 0; i<a.length; i++){ //al valor de a recorrer cada valor de string hasta que se convierta en un valor de tipo number
-        let entero= parseInt (a[i])
-        arrayA.push(entero) // agregar los valores convertidos a number al array vacio 
-        if (i % 2 === 0) {
-        let posicionPar= arrayA[i] * 2 // se realizo la multiplicacion de los numeros pares y se agregaron a la nueva variable
-            console.log(posicionPar)  
-       if (posicionPar >9) {
-          let sumapares = posicionPar.toString().split("")
-          //array de strings (numeros)
-          console.log(sumapares)
-          const primerNumero = parseInt(sumapares[0])
-          const segundoNumero = parseInt(sumapares[1])
-          let sumas= primerNumero + segundoNumero 
-          console.log(sumas)
-          arrayB.push(sumas)
-          console.log (arrayB)
-          
-    //    if (resultado === 0 ) {
-    //         return window.alert ("su tarjeta es valida")
-        
-    //       }
-        }
-        else {
-            arrayB.push(posicionPar)
-            console.log(arrayB)
-        }
-        }
-        else { 
-           let posicionNoPar= arrayA[i]
-           console.log(posicionNoPar)
-           arrayB.push(posicionNoPar)
-           console.log(arrayB)
-        }
-        }
-     let resultado= arrayB.reduce((acc, el) => acc + el, 0)
-     console.log(resultado)
-    }
-    //console.log(arrayA,'este es el array A');
+ console.log(validator);
 
 
-numerosEnteros()
+   const validar= document.getElementById("btn")
+   validar.addEventListener("click", numeroTarjeta) // se creo el evento para el boton y posteriormente se creo una funcion que se pudiera repdroducir al hacer click
+   function numeroTarjeta () { //se creo la funcion en la que se llamo a los valores del input text para que se exportaran al validator y de igual manera el validator regresara la funcion prescrita con los valores dados
+    let tarjeta= document.getElementById("numeros").value
+    validator.isValid(tarjeta)
+    validator.maskify(tarjeta) 
+    const enmascarados= validator.maskify(tarjeta); // se creo una nueva variable que fuera igual a la funcion del maskify
+      document.getElementById("numeros").value = enmascarados // posteriormente se ordeno especificamente que los valores del input fueran igual a la funcion del maskify
+      if (validator.isValid(tarjeta) === true) {
+        alert ("felicidades tu tarjeta es valida")
+      }
+      else {
+        alert ("Tarjeta invalida")
+      }
+   }
+
+
+
+
--- src/validator.js
+++ src/validator.js
@@ -1,6 +1,64 @@
-// const validator = {
-//   // ...
-// };
+//const tarjeta = document.getElementById("numeros")
+const validator = {
+    isValid: function (tarjeta) {
+       //let numeroTarjeta = tarjeta.value
+     //  console.log(numeroTarjeta)
+        let a = tarjeta.split("") //separar valores con el split
+        let arrayA = [] //asignando un array vacio en el que se ingresaran los datos con valor numerico posteriormente
+        let arrayB = []
+        for (let i = 0; i < a.length; i++) { //al valor de a recorrer cada valor de string hasta que se convierta en un valor de tipo number
+            let entero = parseInt(a[i])
+            arrayA.push(entero) // agregar los valores convertidos a number al array vacio 
+            if (i % 2 === 0) {
+                let posicionPar = arrayA[i] * 2 // se realizo la multiplicacion de los numeros pares y se agregaron a la nueva variable
+                console.log(posicionPar)
+               // console.log(typeof arrayA)
+                if (posicionPar > 9) {
+                    let sumapares = posicionPar.toString().split("") //se insertan los valores mayores a 9 en la nueva variable y se convierten y separan en string
+                    //array de strings (numeros)
+                   console.log(sumapares)
+                    const primerNumero = parseInt(sumapares[0]) 
+                    const segundoNumero = parseInt(sumapares[1])
+                    let sumas = primerNumero + segundoNumero // suma de los numeros mayores a 9 seleccionados segun su posicion en el array
+                    arrayB.push(sumas) // se agrega el valor sumado a un nuevo array
+                }
+                else {
+                    arrayB.push(posicionPar) // se agregan al arrayb las posiciones pares restantes que fueron menores a 9
+                 
+                }
+            }
+            else {
+                let posicionNoPar = arrayA[i] // se declara otra variable con los valores no pares que fueron recorridos convertidos a numeros pero no se multiplicaron
+    
+                arrayB.push(posicionNoPar) // se agregan todos los valores al array B
+                
+            }
+        }
+        let resultado = arrayB.reduce((acc, el) => acc + el, 0) 
+       
+        if (resultado % 10 === 0) {
+            return true
+        }
+        else {
+            return false
+        }
+        
+    },
+    maskify (mascara) {
+        let arraymasc= mascara.split("")
+        for (let i = 0; i < arraymasc.length -4; i++) {
+            arraymasc[i]="#"
+        
+        }
+        return arraymasc.join("")
+        
+    }
+   
+    //   // ... 
 
-// export default validator;
+};
+//console.log(validator.maskify())
+
+
+export default validator;
 
